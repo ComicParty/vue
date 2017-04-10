a3481730df860e307f64fecb2aa74869075c39f9
Pati <angerasura@icloud.com>
2017-04-10 23:29:30 +0800

    添加注释

---
+++ vueTyg/meProject/QuestionAnswer.js
@@ -0,0 +1,6 @@
+//main.js下遇到的问题
+
+import router from './router'
+//这句代码引入一段路由配置  但是当然文件夹下只有一个「 router」的文件夹，下面有一个「index.js」难道和服务器中的'index.html'一样，会被自动引用？
+//
+//答：'./router' 这是一个文件夹
--- vueTyg/myProject/src/main.js
+++ vueTyg/myProject/src/main.js
@@ -5,7 +5,9 @@
   // 已经在webpack.base.conf中使用别名设置了//（仅运行时或独立）。   「alias  n. 别名，化名」
   import Vue from 'vue'
   import App from './App'
+  //App.vue这个文件，这是一个视图（或者说组件和页面），想象一下我们的index.html中什么也没有，只有一个视图，这个视图相当于一个容器，然后我们往这个容器中放各种各样的积木（其他组件或者其他页面），App.vue中的内容我们后面说；
   import router from './router'
+  //句代码引入一段路由配置
   // 从“vue”导入Vue
   // 从“./App”导入应用程序
   // 从'./router'导入路由器
@@ -14,14 +16,15 @@
 
   /* eslint-disable no-new */
   // eslint禁用 没有新的
-  new Vue({
-      el: '#app',
+  new Vue({ //new Vue实例化，其实就相当于平时我们写js时候常用的init啦
+      el: '#app', //声明el：'#app'，意思是将所有视图放在id值为app这个dom元素中，
       router,
       template: '<App/>',
-      components: {
+      components: { //components表明引入的文件，即上述的App.vue文件，这个文件的内容将以<App/>这样的标签写进去#app中，
         App
       }
     })
+  // 总的来说，这段代码意思就是将App.vue放到#app中，然后以<App/>来指代我们的#app。
     // 新Vue（{
     //    el：'#app'，
     //    路由器，
@@ -29,5 +32,21 @@
     //    组件：{App}
     //    
 
+  import Vue from 'vue'
+  import App from './App' /*引入App这个组件*/
+  import router from './router' /*引入路由配置*/
 
-  // App.vue这个文件，这是一个视图（或者说组件和页面），想象一下我们的index.html中什么也没有，只有一个视图，这个视图相当于一个容器，然后我们往这个容器中放各种各样的积木（其他组件或者其他页面），App.vue中的内容我们后面说；
+  Vue.config.productionTip = false
+
+  /* eslint-disable no-new */
+  new Vue({
+    el: '#app',
+    /*最后效果将会替换页面中id为app的div元素*/
+    router,
+    /*使用路由*/
+    template: '<App/>',
+    /*告知页面这个组件用这样的标签来包裹着,并且使用它*/
+    components: {
+      App
+    } /*告知当前页面想使用App这个组件*/
+  })
